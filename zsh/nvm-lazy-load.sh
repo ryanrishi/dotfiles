988d460525222b63a77b4eaa2ab90072dda272c5
Ryan Rishi <ryan@ryanrishi.com>
2017-12-18 13:15:40 -0800

    cleanup nvm-lazy-load

--- zsh/nvm-lazy-load.sh
+++ zsh/nvm-lazy-load.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 # http://broken-by.me/lazy-load-nvm/
 nvm() {
-    unset -f nvm
-    [ -s "$(brew --prefix nvm)/nvm.sh" ] && . "$(brew --prefix nvm)/nvm.sh"
-    nvm "$@"
+	if [ -z $NVM_LOADED ]
+	then
+		unset -f nvm
+		[ -s "$(brew --prefix nvm)/nvm.sh" ] && . "$(brew --prefix nvm)/nvm.sh"
+		export NVM_LOADED=1
+		nvm "$@"
+	fi
+
 }
 
 node() {
-    unset -f node
-    [ -s "$(brew --prefix nvm)/nvm.sh" ] && . "$(brew --prefix nvm)/nvm.sh"
-    node "$@"
+	if [ -z $NODE_LOADED ]
+	then
+		unset -f node
+		[ -s "$(brew --prefix nvm)/nvm.sh" ] && . "$(brew --prefix nvm)/nvm.sh"
+		export NODE_LOADED=1
+		node "$@"
+	fi
 }
 
 npm() {
-    unset -f npm
-    [ -s "$(brew --prefix nvm)/nvm.sh" ] && . "$(brew --prefix nvm)/nvm.sh"
-    npm "$@"
+	if [ -z $NPM_LOADED ]
+	then
+		unset -f npm
+		[ -s "$(brew --prefix nvm)/nvm.sh" ] && . "$(brew --prefix nvm)/nvm.sh"
+		export NPM_LOADED=1
+		npm "$@"
+	fi
 }
